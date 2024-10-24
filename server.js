d62d6b46fe45b40acee68fb75be8e572374e1201
SongThang <thangdungdat@gmail.com>
2024-10-24 17:02:09 +0700

    fix api

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ mongoose.connect(process.env.MONGO_URI)
     process.exit(1); // Dừng server nếu không kết nối được
   });
 
-
+// Middleware
 app.use(cors());
 app.use(express.json());
 
@@ -59,6 +59,7 @@ app.get('/api/users', async (req, res) => {
   }
 });
 
+// Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
